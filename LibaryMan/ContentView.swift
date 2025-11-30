36a9f3fa741cb6646826ed81f4d578c897367f8a
Vasiliy <vasyan.coder@gmail.com>
2025-11-30 19:38:06 +0300

    Initial Commit

---
+++ LibaryMan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LibaryMan
+//
+//  Created by Vasyan on 30.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LibaryMan/LibaryManApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibaryManApp.swift
+//  LibaryMan
+//
+//  Created by Vasyan on 30.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct LibaryManApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
